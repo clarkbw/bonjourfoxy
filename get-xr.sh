d854b51e15daba4c4609195baeba158d9e96d408
Bryan <clarkbw@gmail.com>
2010-10-07 13:19:18 -0700

    use wget instead of lynx and use the default mozilla mirror

--- get-xr.sh
+++ get-xr.sh
@@ -5,12 +5,12 @@ pre=xulrunner-${rel}.en-US
 win32=${pre}.win32.sdk.zip
 macppc=${pre}.mac-powerpc.sdk.tar.bz2
 macintel=${pre}.mac-i386.sdk.tar.bz2
-mirrorbase=http://mirror.internode.on.net/pub/mozilla/xulrunner/releases
+mirrorbase=http://releases.mozilla.org/pub/mozilla.org/xulrunner/releases/
 mirrorrel=${mirrorbase}/${rel}/sdk/
 if [ ! $1 ]; then
     echo "No release provided"
     exit 1
-elif [ `lynx -dump ${mirrorbase} | grep -c ${rel}` -ne 2 ]; then
+elif [ `wget -O - ${mirrorbase} | grep -c ${rel}` -ne 1 ]; then
     echo "Check version supplied"
     exit 1
 fi
